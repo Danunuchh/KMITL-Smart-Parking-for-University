218e057c939436185104d5eec1fadcc43b2b47c2
YongsakSaengsuk <chroneo47.dev@gmail.com>
2025-08-04 15:46:43 +0700

    เพิ่มหน้าต่างๆ (navigation)

---
+++ App/smart_parking_for_university/lib/pages/registor_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistorPage extends StatefulWidget {
+  const RegistorPage({super.key});
+
+  @override
+  State<RegistorPage> createState() => _RegistorPageState();
+}
+
+class _RegistorPageState extends State<RegistorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Registor page")));
+  }
+}
