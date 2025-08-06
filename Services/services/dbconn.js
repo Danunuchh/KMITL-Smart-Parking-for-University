673c5d9521f23af58db60d546b3dc9c3268afb60
YongsakSaengsuk <chroneo47.dev@gmail.com>
2025-08-07 04:22:57 +0700

    Database connection

---
+++ Services/services/dbconn.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
+const dbuser = process.env.dbuser;
+const dbpassword = process.env.dbpassword;
+const dbname = process.env.dbname;
+
+// XAMPP
+const pool = mysql.createPool({
+    host: "db",
+    user: dbuser,
+    password: dbpassword,
+    database: dbname,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+module.exports = pool;
